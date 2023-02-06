6d9acfdb9052f73be3f421abd1225b41e0d1be1d
Lara Scavuzzo Montaña <lascavuzzo@gmail.com>
2023-02-06 15:53:16 +0100

    add testing script

--- 04_test.py
+++ 04_test.py
@@ -1,8 +1,44 @@
 import os
 import sys
-import argparse
+import pickle
 import pathlib
+import argparse
 import numpy as np
+
+
+def svmrank_process(policy, data_loader, top_k=[1, 3, 5, 10]):
+  mean_kacc = np.zeros(len(top_k))
+  n_samples_processed = 0
+
+  for batch in data_loader:
+    feats, best, num_cands = batch
+    n_samples = best.shape[0]
+
+    # feature normalization
+    feats = (feats - policy['shift']) / policy['scale']
+
+    # prediction
+    pred_scores = policy['model'].predict(feats)
+    pred_scores = torch.FloatTensor(pred_scores)
+    pred_scores = pad_tensor(pred_scores, num_cands)
+
+    # accuracy
+    kacc = []
+    for k in top_k:
+      if pred_scores.size()[-1] < k:
+        kacc.append(1.0)
+        continue
+      pred_top_k = pred_scores.topk(k).indices
+      accuracy = (pred_top_k == best).any(dim=-1).float().mean().item()
+      kacc.append(accuracy)
+    kacc = np.asarray(kacc)
+
+    mean_kacc += kacc * n_samples
+    n_samples_processed += n_samples
+
+  mean_kacc /= n_samples_processed
+  return mean_kacc
+    
 
 
 def gnn_process(policy, data_loader, top_k=[1, 3, 5, 10]):
@@ -101,7 +137,7 @@ if __name__ == "__main__":
       ## load data ##
       test_files = [str(file) for file in (pathlib.Path(f'data/samples')/problem_folder/'test').glob('sample_*.pkl')]
       test_data = GraphDataset(test_files)
-      test_loader = torch_geometric.loader.DataLoader(test_data, 128, shuffle=False)
+      test_loader = torch_geometric.loader.DataLoader(test_data, 64, shuffle=False)
 
       ## load gnn model ##
       model = GNNPolicy().to(device)
@@ -113,13 +149,33 @@ if __name__ == "__main__":
       log(f"TEST RESULTS: " + "".join([f" acc@{k}: {acc:0.3f}" for k, acc in zip(top_k, test_kacc)]), logfile)
 
     else:
+      import torch
       import svmrank
+      from utilities import log, FlatDataset, pad_tensor
 
-      # load feature normalization parameters #
-      with open(f"trained_models/{args.problem}/0/normalization.pkl", 'rb') as f:
-        feat_shift, feat_scale = pickle.load(f)
+      ## load feature normalization parameters ##
+      policy = {}
+      with open(f"{model_dir}/normalization.pkl", 'rb') as f:
+        policy['shift'], policy['scale'] = pickle.load(f)
+        print(policy['shift'], policy['scale'])
 
-      # load model #
-      model = svmrank.Model().read(f"trained_models/{args.problem}/0/model.txt")
+      ## load data ##
+      test_files = [str(file) for file in (pathlib.Path(f'data/samples')/problem_folder/'test').glob('sample_*.pkl')]
+      test_data = FlatDataset(test_files)
+      test_loader = torch.utils.data.DataLoader(test_data, 64, shuffle=False, collate_fn=FlatDataset.collate)
+
+      ## load model ##
+      policy['model'] = svmrank.Model().read(f"{model_dir}/model.txt")
+
+      ## test ##
+      top_k = [1, 3, 5, 10]
+      test_kacc = svmrank_process(policy, test_loader)
+      log(f"TEST RESULTS: " + "".join([f" acc@{k}: {acc:0.3f}" for k, acc in zip(top_k, test_kacc)]), logfile)
+
+
+      
+
+    
+
 
     
